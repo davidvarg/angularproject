a1c838afcce97baac500a87be992d43f3927238d
david vargas <davidvar7@gmail.com>
2018-01-31 15:05:25 -0600

    adding minor changes in styles for service component

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css']
+  //styleUrls: ['./service.component.css']
+  styles:['h4{ color:darkblue; }']
 })
 export class ServiceComponent implements OnInit {
 
